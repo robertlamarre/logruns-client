f6114e1e7b0966d9acffa77d44804038042acd93
pcavanaugh <pcavanaugh999@gmail.com>
2014-04-20 23:29:16 -0400

    made the landing page slightly less ugly

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,10 +1,42 @@
 'use strict';
 
 angular.module('logrunsApp')
-  .controller('HomeCtrl', function ($scope, user) {
+  .controller('HomeCtrl', function ($scope, user, $location) {
+    $scope.account = 'Sign in';
+
+    user.getUser({
+      success: function(data) {
+        $scope.account = data.local.username;
+      }
+    });
+
     user.getUsers({
       success: function(data) {
         $scope.users = data;
       }
     });
+
+    var login = function() {
+      $location.path('/login');
+    };
+
+    var logout = function() {
+      $scope.account = 'Sign in';
+      user.logout({
+        success: function() {
+          console.log('Successfully signed out');
+        },
+        error: function() {
+          console.log('Error signing out');
+        }
+      });
+    };
+
+    $scope.logInOut = function() {
+      if ($scope.account.toLowerCase() === 'sign in') {
+        login();
+        return;
+      }
+      logout();
+    };
   });

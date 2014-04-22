650067e5d4deed0a34ef9134f8018c9e3dcaf842
pcavanaugh <pcavanaugh999@gmail.com>
2014-04-21 23:52:42 -0400

    so much for not staying up all night...

--- app/scripts/controllers/newentry.js
+++ app/scripts/controllers/newentry.js
@@ -3,13 +3,22 @@
 angular.module('logrunsApp')
   .controller('NewEntryCtrl', function ($scope, $location, user) {
     $scope.entry = {};
+
+    user.getUser({
+      success: function(data) {
+        $scope.user = data;
+      }
+    });
+
     $scope.submit = function() {
       console.log($scope.entry);
       user.postEntry({
         entry: $scope.entry,
         success: function(data) {
           console.log(data);
-          $location.path('/');
+          if ($scope.user) {
+            $location.path('/u/' + $scope.user.local.username);
+          }
         },
         error: function(data) {
           console.error('busted', data);

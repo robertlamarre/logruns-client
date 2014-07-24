2f0ce8fce86e9fdac84b6d7c688cc7be3f589678
pcavanaugh <pcavanaugh999@gmail.com>
2014-07-23 21:52:35 -0400

    highlight search results

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,13 +1,20 @@
 'use strict';
 
 angular.module('logrunsApp')
-  .controller('SearchCtrl', function ($scope, $routeParams, user) {
+  .controller('SearchCtrl', function ($scope, $routeParams, $timeout, user) {
     $scope.text = $routeParams.text;
 
     user.searchEntriesForText({
       text: $routeParams.text,
       success: function(data) {
         $scope.entries = data;
+        $timeout(function() {
+          var regex = new RegExp('(' + $scope.text + ')', 'i');
+          $('.blockquote').each(function(idx, val) {
+            var html = $(val).html().replace(regex, '<span class="hit">$1</span>');
+            $(val).html(html);
+          });
+        });
       }
     });
 
